6badcd2f4cdce900555b2317e3ed5f522e3f1325
Peter Sigurdson <petersigurdson@Peters-MacBook-Air.local>
2020-02-02 10:15:18 -0500

    2nd commit

---
+++ PSModalStatus/PSModalStatusView.swift
@@ -0,0 +1,21 @@
+//
+//  PSModalStatusView.swift
+//  PSModalStatus
+//
+//  Created by Peter Sigurdson on 2020-02-02.
+//  Copyright © 2020 theprof. All rights reserved.
+//
+
+import UIKit
+
+class PSModalStatusView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
