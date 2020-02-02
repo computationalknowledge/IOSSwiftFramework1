7c80fe7fbb39b09494be82f059fa552aa21c639d
Peter Sigurdson <petersigurdson@Peters-MacBook-Air.local>
2020-02-02 10:06:09 -0500

    first commit

---
+++ PSModalStatus/PSModalStatus/PSModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  PSModalStatus.h
+//  PSModalStatus
+//
+//  Created by Peter Sigurdson on 2020-02-02.
+//  Copyright © 2020 theprof. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PSModalStatus.
+FOUNDATION_EXPORT double PSModalStatusVersionNumber;
+
+//! Project version string for PSModalStatus.
+FOUNDATION_EXPORT const unsigned char PSModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PSModalStatus/PublicHeader.h>
+
+
